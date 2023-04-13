41a9284fc1cd145d3738670b94cc00182adb1d76
defx <mdonkin83@gmail.com>
2023-04-13 10:02:06 +0100

    export subscribe from s4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { render } from "./r5.js"
 import { getState, dispatch, subscribe, ready } from "./s4.js"
 
 export { html, render } from "./r5.js"
-export { configure } from "./s4.js"
+export { configure, subscribe } from "./s4.js"
 
 export const define = (name, fn) => {
   if (customElements.get(name)) return
