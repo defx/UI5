0f526b9847ed831b0dfae9b3ae34c4b88cb3aa03
defx <mdonkin83@gmail.com>
2023-05-17 16:15:08 +0100

    tidy

--- src/index.js
+++ src/index.js
@@ -7,24 +7,11 @@ export { createStore } from "./s4.js"
 export const define = (name, templateFn, reducer = () => ({})) => {
   if (customElements.get(name)) return
 
-  let c = 0
-
   customElements.define(
     name,
     class extends HTMLElement {
       async connectedCallback() {
-        const ns = this.getAttribute("ns") || `${name}/${++c}`
         const store = createStore(reducer)
-
-        this.dispatchEvent(
-          new CustomEvent("c8/subscribe", {
-            bubbles: true,
-            detail: {
-              ns,
-              store,
-            },
-          })
-        )
 
         const update = (state) =>
           render(templateFn(state, store.dispatch), this)
